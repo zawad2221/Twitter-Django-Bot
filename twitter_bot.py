4dbce40234baa0d695cc086a02e035d412fe2faa
zawad2221 <rifat2221@outlook.com>
2020-12-10 02:21:15 +0600

    complete

---
+++ twitter_bot.py
@@ -0,0 +1,58 @@
+import tweepy
+import json
+
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+       
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        if not tweet.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+                print("liked tweet: ",tweet.text)
+            except Exception as e:
+                print("Error on fav",e)
+        # if not tweet.retweeted:
+        #     # Retweet, since we have not retweeted it yet
+        #     try:
+        #         tweet.retweet()
+        #     except Exception as e:
+        #         logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status):
+        print(status)
+
+hashtag = ["#Django", "#django", "#DjantweetgoDev", "#djangodev",
+ "#python", "#djangounchained", "#pythonprogramming",
+  "#DjangoDevelopment", "#PythonProgramming",
+ "#djangodevelopment","#DjangoDevelopment","#DjangoDev",
+ "#androiddevelopment","#androiddev","#AndroidDev","#AndroidDevelopment",
+ "#androidstudio","#java","#Java",
+ "#javaprogramming","#JavaProgramming","#JavaDevelopment",
+ "#javadevelopment","#kotlin","#Kotlin","#kotlindev","#KotlinDev",
+ "#kotlindevelopment","#AndroidDev"]
+
+def main(keywords):
+    auth = tweepy.OAuthHandler("key", "secret")
+    auth.set_access_token("key", "secret")
+    api = tweepy.API(auth)
+    try:
+        api.verify_credentials()
+        print("authentication ok")
+    except:
+        print("error during authentication")
+        return
+
+    tweets_listener = FavRetweetListener(api)
+    stream = tweepy.Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["en"])
+
+main(hashtag)
