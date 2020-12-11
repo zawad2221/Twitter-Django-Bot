7e9d7c0cf09f2d09554daa2c9439ab586f213911
zawad2221 <rifat2221@outlook.com>
2020-12-11 20:59:08 +0600

    create separate file for like friends tweet

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python twitter_bot.py
+worker: like_friends_tweet.py
---
+++ like_friends_tweet.py
@@ -0,0 +1,59 @@
+import tweepy
+import json
+from os import environ
+
+def main():
+    # CONSUMER_KEY = environ['CONSUMER_KEY']
+    # CONSUMER_SECRET = environ['CONSUMER_SECRET']
+
+    # ACCESS_KEY = environ['ACCESS_KEY']
+    # ACCESS_SECRET = environ['ACCESS_SECRET']
+
+    # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    # auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    auth = tweepy.OAuthHandler("ohzLCn0emBTqTBVYxHyz36GNT","L6RNDzx1QHdKr2aOibRTjVRSkMY2KrFov2Gd4za1LApwPC52cO")
+    auth.set_access_token("1301556273993793536-6EsHUDQrpepboAiD5W2frrhtWnFKGg","lKxZJPUlgG3GOSnQOo99COd0HBIfQufY1XapNGg2v4VVd")
+    
+    
+    api = tweepy.API(auth)
+    try:
+        api.verify_credentials()
+        print("authentication ok")
+    except:
+        print("error during authentication")
+        return
+
+
+    followers = []
+    print("after followers dic")
+    for page in tweepy.Cursor(api.friends, screen_name='@ZawadHossain12', wait_on_rate_limit=True).pages():
+        try:
+            followers.extend(page)
+        except tweepy.TweepError as e:
+            print("Going to sleep:", e)
+        print("after loop1")
+        for user in followers:
+            try:
+                print("---------------------------------")
+                print("user name:",user.name)
+                tweets = api.user_timeline(id=user.id)
+                #number of tweet i want to like
+                limit = 4
+                count = 0
+                for tweet in tweets:
+                    # api.create_favorite(tweet.id)
+                    if not tweet.favorited:
+                        try:
+                            tweet.favorite()
+                            print('----Liking Tweet-----', tweet.text)
+                            print(tweet.favorited)
+                            count +=1
+                        except Exception as e:
+                            print("erron in liking tweet, error: ",e)
+                    if count >=limit:
+                        break
+            except Exception as e:
+                print("error in getting tweet, error: ",e)
+
+while(True):
+    main()
