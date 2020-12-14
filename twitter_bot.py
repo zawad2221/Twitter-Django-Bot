761bb0f071f81548c669ea8ae1a8297b84afbb4d
zawad hossain <32180355+zawad2221@users.noreply.github.com>
2020-12-14 15:14:29 +0600

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -14,7 +14,7 @@ class FavRetweetListener(tweepy.StreamListener):
             tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
             return
-        if not tweet.favorited:
+        if tweet.favorited == False:
             # Mark it as Liked, since we have not done it yet
             try:
                 tweet.favorite()
