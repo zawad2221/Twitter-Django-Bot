4f4bc0a62bd59196a9ba002e6f54fb14693ad69a
zawad hossain <32180355+zawad2221@users.noreply.github.com>
2021-01-03 20:04:47 +0600

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -21,34 +21,15 @@ class FavRetweetListener(tweepy.StreamListener):
                 print("liked tweet: ")
             except Exception as e:
                 print("Error on fav",e)
-                time.sleep(9)
+                #time.sleep(9)
 
 
-                # followers = []
-                # print("after followers dic")
-                # for page in tweepy.Cursor(self.api.friends, screen_name='@ZawadHossain12', wait_on_rate_limit=True,count=10).pages():
-                #     try:
-                #         followers.extend(page)
-                #     except tweepy.TweepError as e:
-                #         print("Going to sleep:", e)
-                # print("after loop1")
-                # for user in followers:
-                #     try:
-                #         print(user.name)
-                #         tweet = api.user_timeline(id = user.id, count = 1)[0]
-                #         #api.create_favorite(tweet.id)
-                #         if not tweet.favorited:
-                #             tweet.favorite()
-                #             print('----Liking Tweet-----',tweet.text)
-                #             print(tweet.favorited)
-                #     except:
-                #         print("error in liking following tweet")
-        # if not tweet.retweeted:
-        #     # Retweet, since we have not retweeted it yet
-        #     try:
-        #         tweet.retweet()
-        #     except Exception as e:
-        #         logger.error("Error on fav and retweet", exc_info=True)
+        if not tweet.retweeted:
+             # Retweet, since we have not retweeted it yet
+             try:
+                 tweet.retweet()
+             except Exception as e:
+                 print("Error on retweet",e)
 
     def on_error(self, status):
         print(status)
@@ -57,11 +38,7 @@ hashtag = ["#Django", "#django", "#DjantweetgoDev", "#djangodev",
  "#python", "#djangounchained", "#pythonprogramming",
   "#DjangoDevelopment", "#PythonProgramming",
  "#djangodevelopment","#DjangoDevelopment","#DjangoDev",
- "#androiddevelopment","#androiddev","#AndroidDev","#AndroidDevelopment",
- "#androidstudio","#java","#Java",
- "#javaprogramming","#JavaProgramming","#JavaDevelopment",
- "#javadevelopment","#kotlin","#Kotlin","#kotlindev","#KotlinDev",
- "#kotlindevelopment","#AndroidDev","100DaysOfCode","100daysofcode","100daysofcodechallenge"]
+ "#100DaysOfCode","100daysofcode","100daysofcodechallenge"]
 
 def main(keywords):
     CONSUMER_KEY = environ['CONSUMER_KEY']
